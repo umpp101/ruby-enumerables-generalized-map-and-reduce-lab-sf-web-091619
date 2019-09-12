03de2f5c2bffddee020eb277448715f19afffc0a
Mustafa Muse <umpp101@icloud.com>
2019-09-12 18:48:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length do
+    new_array.push(yield(array[i]))
+  i += 1 
+end
+new_array
+end
+
+
+def reduce(array, starting_point = 0)
+  total = starting_point
+  i = 0 
+  while i < array.length do
+    total = yield(array[i], total)
+  i += 1 
+end
+total 
+end 
